9507d98ca15e5dd72fda421cafcc3370be79a74e
tjy86 <thomasjyang@gmail.com>
2012-12-07 14:22:22 -0500

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAnalytics::Application.config.secret_token = '228047c70d104ddc9abb22b9412e61574c56cd47024293ca8881c7ca83477eb087fe439a379f6fc628eb35f279c6602bd258bec154956c528ec13769e5e11323'
